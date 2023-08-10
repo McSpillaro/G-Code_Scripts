65f9e248323102b5bfea3410a176a078c9e85e58
Ernest Spiller <e.spiller2002@gmail.com>
2023-08-09 18:30:01 -0700

    Yes

--- X_HATCH_10L_d2_dz_0.01_dt15_F250.gcode
+++ X_HATCH_10L_d2_dz_0.01_dt15_F250.gcode
@@ -13,3 +13,81 @@ T1;
 ;; Layer 0
 M790
 
+   G   X   Y    Z E F
+0  0            3    
+1  0  60  20         
+2  0          0.0    ;; Priming;
+G0 Z0.0;
+G0 ;; Layer 1
+M790
+
+   G   X   Y     Z E F
+0  0             3    
+1  0  60  20          
+2  0          0.01    ;; Priming;
+G0 Z0.01;
+G0 ;; Layer 2
+M790
+
+   G   X   Y     Z E F
+0  0             3    
+1  0  60  20          
+2  0          0.02    ;; Priming;
+G0 Z0.02;
+G0 ;; Layer 3
+M790
+
+   G   X   Y     Z E F
+0  0             3    
+1  0  60  20          
+2  0          0.03    ;; Priming;
+G0 Z0.03;
+G0 ;; Layer 4
+M790
+
+   G   X   Y     Z E F
+0  0             3    
+1  0  60  20          
+2  0          0.04    ;; Priming;
+G0 Z0.04;
+G0 ;; Layer 5
+M790
+
+   G   X   Y     Z E F
+0  0             3    
+1  0  60  20          
+2  0          0.05    ;; Priming;
+G0 Z0.05;
+G0 ;; Layer 6
+M790
+
+   G   X   Y     Z E F
+0  0             3    
+1  0  60  20          
+2  0          0.06    ;; Priming;
+G0 Z0.06;
+G0 ;; Layer 7
+M790
+
+   G   X   Y     Z E F
+0  0             3    
+1  0  60  20          
+2  0          0.07    ;; Priming;
+G0 Z0.07;
+G0 ;; Layer 8
+M790
+
+   G   X   Y     Z E F
+0  0             3    
+1  0  60  20          
+2  0          0.08    ;; Priming;
+G0 Z0.08;
+G0 ;; Layer 9
+M790
+
+   G   X   Y     Z E F
+0  0             3    
+1  0  60  20          
+2  0          0.09    ;; Priming;
+G0 Z0.09;
+G0 
--- cross_hatch.py
+++ cross_hatch.py
@@ -14,29 +14,41 @@ import pandas as pd
 size = input('Enter width of square [cm] (Default: 2): ')
 if size == '':
     size = 2
-    
+
 num_layer = input('Enter number of layers (Default: 10): ')
 if num_layer == '':
     num_layer = 10
-    
+
 print_speed = input('Enter print speed [mm/min] (Default: 250): ')
 if print_speed == '':
     print_speed = 250
-    
-dz = input('Enter step height [mm] (Default: 0.01): ')   
+
+dz = input('Enter step height [mm] (Default: 0.01): ')
 if dz == '':
     dz = 0.01
-    
+
 dwell_time = input('Enter time between layers [s] (Default: 15): ')
 if dwell_time == '':
     dwell_time = 15
 
+# Formats the .gcode file from data frame input
+def pd_df_formatter(dataframe):
+    list_data = []
+    df = dataframe.iloc[:, :]
+
+    try:
+        for b, a in df:  # loops through each row in the given print pattern dataframe
+            list_data.append(f'{a}')
+        print(list_data)
+    except:
+        print('Could not format dataframe.') 
+    return
+
 # Creating the .gcode file
 with open(f'X_HATCH_{num_layer}L_d{size}_dz_{dz}_dt{dwell_time}_F{print_speed}.gcode', 'w') as file:
-    size *= 10 # cm -> mm
-    center = 80 # distance between centers of circles
-    bands = round(size) # keeps the number of bands a whole number
-
+    size *= 10  # cm -> mm
+    center = 80  # distance between centers of circles
+    bands = round(size)  # keeps the number of bands a whole number
 
     # Sets initial parameters
     file.write('G21          ; Sets units to millimeters\n')
@@ -57,33 +69,52 @@ with open(f'X_HATCH_{num_layer}L_d{size}_dz_{dz}_dt{dwell_time}_F{print_speed}.g
     for layer in range(num_layer):
         # New layer
         file.write(f';; Layer {layer}\n')
-        file.write('M790\n\n') # Displays new layer on printer
-        
-        z = layer * dz # Sets the z-pos based on layer number
-        
+        file.write('M790\n\n')  # Displays new layer on printer
+
+        z = (layer + 1) * dz  # Sets the z-pos based on layer number
+
         # Array of commands
-        priming = pd.DataFrame(
-        data=[ ['abc', 3.3, 28, True],
-                ['xyz', -.55, 0, False] ], 
-        columns=['Label1', 'Label2', 'Label3', 'Label4'],
-        index=[0, 1] 
-        )
+        # Create a pandas dataframe that contains the dictionary of commands and position values
+        # Create a dictionary that contains the cmds and pos values and is loopable from params.
+        priming_df = pd.DataFrame()
         
-        pattern_1 = pd.DataFrame(
+        for i in len()
+        G_dict = {
+            ['G':]
+        }
+        
+        priming_command_list = []
+        for i in range(size):
             
+        priming_dict = {
+            'G':
+        }
+        
+        priming = pd.DataFrame(
+            data=[[]],
+            columns=['G', 'X', 'Y', 'Z', 'E', 'F']
         )
-        pattern_2 = pd.DataFrame(
-            
+        horizontal_pattern = pd.DataFrame(
+            data=[[]]
         )
-        pattern_3 = pd.DataFrame(
-            
+        LX_patter = pd.DataFrame(
+            data=[[]]
         )
-        pattern_4 = pd.DataFrame(
-            
+        vertical_pattern = pd.DataFrame(
+            data=[[]]
+        )
+        RX_pattern = pd.DataFrame(
+            data=[[]]
         )
 
         # Primes the printer
-        file.write(priming.iloc(,'Label1'))
+        file.write(priming)
+        # file.write((pd_df_formatter(priming)))
         file.write(';; Priming;\n')
-        file.write(f'G0 Z{z};\n') # Sets needle to the desired print height
+        file.write(f'G0 Z{z};\n')  # Sets needle to the desired print height
         file.write(f'G0 ')
+
+'''
+When printing, have the first layer at a flow rate of 2.000 and the succeeding layers be
+a flow rate of 0.250 to allow for optimal printing quality with wt% 60 and below.
+'''
